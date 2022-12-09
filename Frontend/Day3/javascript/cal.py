087fea5b402ff686f0b504293875eee44c0f410d
Seung Hyun Yoon <118493648+ysh21368@users.noreply.github.com>
2022-12-09 17:03:17 +0900

    Add files via upload

---
+++ Frontend/Day3/javascript/cal.py
@@ -0,0 +1,54 @@
+def cal(oper,num1,num2):
+    
+    if len(args) >= 2: 
+
+
+
+
+
+
+
+
+    if oper == "add":
+        temp = num1 + num2
+    elif oper == 'sub':
+        temp =  num1-num2
+    elif oper == 'mul':
+        temp = num1*num2
+    elif oper == 'div':
+        temp =  num1/num2
+
+    return temp
+
+
+
+def add(num1, num2):
+    temp = x + y
+    return temp
+
+def sub(x, y):
+    temp =  x - y
+    return temp
+    
+def mul(x, y):
+    temp = x*y
+    return temp
+
+def div(x, y):
+    temp = x / y
+    return temp
+
+def print_result(result):
+    print(result)
+
+def main():
+    mul(cal('add',1,2), 3)
+
+
+
+result = cal('mul',cal('add',1,2),2)
+print_result(result)
+
+cal('mul',1,2).add(3).mul(2)
+mul(1).add(3).mul(1).
+add('ca',1,2,3,4,5)
